4fdfa458250e3bb627509035ac5fab303fca0f10
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-02-26 13:47:44 -0500

    created the code to start the clock

--- clock/ViewController.swift
+++ clock/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //weak is xcode will not hold any reference to this object if it is not required. 
+    //strong always remain there. 
+  
+    @IBOutlet weak var clock: UILabel!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let myNum = 5
+        clock.text = "Updated"
+        
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
+        
     }
 
+    
+    func updateClock() {
+        print("Updated From Clock")
+        
+    }
 
 
 }
