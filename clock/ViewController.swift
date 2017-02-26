91c5f01ff09bd644a25d74ea3c078a9f861eeb07
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-02-26 12:50:18 -0500

    created a simple UI

---
+++ clock/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  clock
+//
+//  Created by Sam Greenhill on 2/26/17.
+//  Copyright © 2017 simplyAmazingMachines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
