5e5a7ba291b20fa8fe095b27d59bd0c13bc36a13
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-02-26 14:09:53 -0500

    we created the code that updated the clock

--- clock/ViewController.swift
+++ clock/ViewController.swift
@@ -17,13 +17,13 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        
+        updateClock()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let myNum = 5
-        clock.text = "Updated"
+        
+        clock.text = ""
         
         
         Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
@@ -32,8 +32,37 @@ class ViewController: UIViewController {
 
     
     func updateClock() {
-        print("Updated From Clock")
         
+        let dateNow: NSDate = NSDate()
+        let calendar = NSCalendar.current
+        let components = calendar.dateComponents([.hour, .minute, .second], from: dateNow as Date)
+        
+        let hour: Int = (components.hour! > 12 ? components.hour! - 12 : components.hour)!
+        
+        //the statement above can be written as an if else statement, which it is below
+//        var hours: Int = 0
+//        if components.hour! > 12 {
+//            hours = components.hour! - 12
+//        } else {
+//            hour = components.hour!
+//        }
+        
+        
+        
+        let hourString: String = hour > 9 ? "\(hour)" : "0\(hour)"
+        
+        let minutes = components.minute! > 9 ? "\(components.minute!)" : "0\(components.minute!)"
+        let seconds = components.second! > 9 ? "\(components.second!)" : "0\(components.second!)"
+        
+        let am = components.hour! > 12 ? "PM" : "AM"
+        
+        let timeString = "\(hourString):\(minutes):\(seconds):\(am)"
+       
+        //print(components.hour!)
+        //will be an optional if there is no !
+        
+        
+        clock.text = timeString
     }
 
 
